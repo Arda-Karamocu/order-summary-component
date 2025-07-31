c3d547223bc5b27f552d4268a93afc89da09994d
Arda Karamoçu <140858696+Arda-Karamocu@users.noreply.github.com>
2025-07-31 19:39:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ function App() {
   return (
     <div className="bg-[url('src/images/pattern-background-mobile.svg')] lg:bg-[url('src/images/pattern-background-desktop.svg')] bg-no-repeat bg-top bg-contain min-h-screen w-full bg-[#e0e8ff] font-RedHatDisplay flex items-center justify-center py-10 lg:py-0">
       <div className="flex flex-col items-center justify-center bg-white rounded-xl shadow-2xl overflow-hidden w-[90%] max-w-md gap-6">
-        {/* Hero Image */}
         <img
           src="src/images/illustration-hero.svg"
           alt="illustration-hero"
           className="w-full"
         />
 
-        {/* Card Content */}
         <div className="flex flex-col items-center justify-center gap-6 px-6 pb-8 text-center">
           <h2 className="font-[900] text-2xl text-[hsl(223,47%,23%)]">
             Order Summary
@@ -19,7 +17,6 @@ function App() {
             any device anywhere you like!
           </p>
 
-          {/* Plan Row */}
           <div className="flex flex-row items-center justify-between bg-[hsl(225,100%,98%)] py-4 px-5 rounded-xl w-full">
             <div className="flex items-center gap-4">
               <img
@@ -44,7 +41,6 @@ function App() {
             </a>
           </div>
 
-          {/* Buttons */}
           <button className="bg-[hsl(245,75%,52%)] hover:bg-[hsl(245,83%,68%)] text-white font-[700] w-full py-3 rounded-xl shadow-lg transition">
             Proceed to Payment
           </button>
